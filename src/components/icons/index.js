ec53b9b85d54418e9a7ed943ce13fd1050a768b1
Juan Diego <102696740+juandiego252@users.noreply.github.com>
2025-01-17 00:27:07 -0500

    Portafolio Juan Diego Updated

---
+++ src/components/icons/index.js
@@ -0,0 +1,7 @@
+export { default as GitHub } from './GitHub.astro';
+export { default as LinkedIn } from './LinkedIn.astro';
+export { default as Mail } from './Mail.astro';
+export { default as AboutMe } from './AboutMe.astro';
+export { default as Proyects } from './Proyects.astro';
+export { default as File } from './File.astro';
+// Añade aquí más exportaciones según sea necesario
---
+++ src/components/icons/projects-icons/index.js
@@ -0,0 +1,3 @@
+export {default as NextJS} from './NextJS.astro';
+export {default as Tailwind} from './Tailwind.astro';
+export {default as React} from './React.astro';
