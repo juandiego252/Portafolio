4e73cb1a0661a23c1f8797df6208fcaecee61a6b
juandiego252 <juandiegojd252@gmail.com>
2025-08-11 15:02:29 -0500

    fix: tailwindcss dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 
+
 // https://astro.build/config
 export default defineConfig({
   i18n: {
@@ -11,6 +12,8 @@ export default defineConfig({
       prefixDefaultLocale: false,
     }
   },
-  integrations: [tailwindcss()],
+    vite: {
+    plugins: [tailwindcss()],
+  },
   site: "https://juandiegc.me"
 });
